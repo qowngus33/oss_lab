ed1d4065016f8217bf946351509b088a0ca4897a
qowngus33 <qowngus33@naver.com>
2022-09-22 12:50:10 +0900

    add code to skeleton file

--- class_score_analysis_skeleton.py
+++ class_score_analysis_skeleton.py
@@ -1,15 +1,50 @@
 def read_data(filename):
     data = []
-    # TODO
+    try:
+        with open(filename, 'r') as fi:
+            for line in fi.readlines():
+                try:
+                    if line[0] != '#':
+                        values = [int(text) for text in line.split(',')]
+                        temp = []
+                        for val in values:
+                            temp.append(int(val))
+                        data.append(temp)
+                except ValueError as ex:  # Try 'data/class_score_en.csv' without this try/except
+                    print(f'A line is ignored.(message: {ex})')
+    except Exception as ex:
+        print(f'Cannot run the program. (message: {ex})')
+    return data
 
 def add_weighted_average(data, weight):
     for row in data:
-        row.append(0)   # TODO
+        row.append(row[0]*weight[0] + row[1]*weight[1])
+
+def mean_var(data):
+    n = len(data)
+    if n > 0:
+        mean = sum(data) / n
+        sum2 = sum([datum**2 for datum in data])
+        var = sum2 / n - mean**2
+        return mean, var
+    return None, None
 
 def analyze_data(data):
-    mean = 0            # TODO
-    var = 0             # TODO
-    median = 0          # TODO
+    n = len(data)
+    mean = 0
+    var = 0
+    median = 0
+    if n > 0:
+        mean = sum(data) / n
+        sum2 = sum([datum ** 2 for datum in data])
+        var = sum2 / n - mean ** 2
+    data.sort()
+
+    if len(data) % 2 == 0:
+        median = (data[len(data)/2-1]+data[len(data/2)])/2
+    else:
+        median = data[len(data)//2]
+
     return mean, var, median, min(data), max(data)
 
 if __name__ == '__main__':
